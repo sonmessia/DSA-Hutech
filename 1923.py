664069585e4b21169042988426d47adb6e4f69ef
Hoang Son <sonmessia123@gmail.com>
2025-03-11 22:57:39 +0700

    Giai ti bai itcoder

--- 1923.py
+++ 1923.py
@@ -1,47 +1,40 @@
-import math
 from functools import reduce
+import math
+def lcm(x, y):
+    return x * y // math.gcd(x, y)
 
-def get_divisors(n):
+def get_divisor(n: int) -> list:
     divisors = []
-    for i in range(1, int(math.sqrt(n)) + 1):
+    for i in range(1, int(n ** 0.5) + 1):
         if n % i == 0:
             divisors.append(i)
             if i != n // i:
                 divisors.append(n // i)
     return sorted(divisors)
 
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
+def binary_search(arr: list, target: int) -> int:
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
         if arr[mid] >= target:
-            right = mid - 1
+            high = mid - 1
         else:
-            left = mid + 1
-    return left
-
-def count_valid_x(A, B):
-
+            low = mid + 1
+    return low
+def solve(A: list, B: list) -> int:
     gcd_a = reduce(math.gcd, A)
-
-    lcm_b = reduce(lambda x, y: x * y // math.gcd(x, y), B)
-
-    if lcm_b > gcd_a:
-        return 0
-
-    divisors = get_divisors(gcd_a)
-
-    start_idx = binary_search(divisors, lcm_b)
-
-    count = 0
-    for i in range(start_idx, len(divisors)):
+    lcm_b = reduce(lambda x, y: lcm(x, y), B)
+    cnt = 0
+    divisors = get_divisor(gcd_a)
+    
+    startIndex = binary_search(divisors, lcm_b)
+    
+    for i in range(startIndex, len(divisors)):
         if divisors[i] % lcm_b == 0:
-            count += 1
-            
-    return count
+            cnt += 1
+    return cnt
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
-ans = count_valid_x(A, B)
-print(ans)
+print(solve(A, B))
